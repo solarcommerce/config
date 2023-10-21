d2297fe90ced9ecbc30af18b5e9255920b27b040
Jacek Nowak <jaceknowak226@gmail.com>
2023-10-21 12:23:40 +0200

    ci: change rollup config back to js file

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,32 +1,27 @@
-import { RollupOptions } from 'rollup';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
 
-import packageJson from './package.json' assert { type: 'json' };
-
-const config: RollupOptions[] = [
+export default [
   {
     input: 'src/index.ts',
     output: [
       {
-        file: packageJson.main,
+        file: 'dist/cjs/index.js',
         format: 'cjs',
         sourcemap: true,
       },
       {
-        file: packageJson.module,
+        file: 'dist/esm/index.js',
         format: 'esm',
         sourcemap: true,
       },
     ],
-    plugins: [commonjs(), typescript({ tsconfig: './tsconfig.json' })],
+    plugins: [commonjs(), typescript()],
   },
   {
-    input: 'dist/esm/types/src/index.d.ts',
+    input: 'dist/esm/types/index.d.ts',
     output: [{ file: 'dist/index.d.ts', format: 'esm' }],
     plugins: [dts()],
   },
 ];
-
-export default config;
